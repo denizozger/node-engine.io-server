12cc20436de44dfaa39f1498c56cb1dd3fc6e37f
Deniz Ozger <deniz@whoscored.com>
2014-01-31 17:12:30 +0000

    Multi CPU support

---
+++ one-process-server.js
@@ -0,0 +1,207 @@
+'use strict';
+
+const express = require('express'),
+  app = express(),
+  http = require('http'),
+  engine = require('engine.io'),
+  zmq = require('zmq'),
+  redis = require('redis'),
+  redisClient = redis.createClient(),
+  Q = require('q'),
+  log = require('npmlog'),
+  strongloop = require('strong-agent').profile();
+
+log.level = process.env.LOGGING_LEVEL || 'verbose';
+
+// Set how many concurrent sockets http agent can have open per host
+http.globalAgent.maxSockets = Infinity;
+process.setMaxListeners(0);
+
+// http server
+const server = http.createServer(app);
+
+// WebSocket server
+const io = engine.attach(server);
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res, next){
+  res.sendfile('index.html');
+});
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, function(){
+  log.info('Web socket server (Worker ' + process.pid + ') is listening on ', port);
+});
+
+/**
+ * Data structures
+ */
+
+ // These are (currently) redis clients subscribed to different channels
+ var resourceSubscribers = {};
+
+/**
+ * Public Endpoints
+ */
+ 
+io.on('connection', function (socket) {
+  handleClientConnected(socket);
+});
+
+function handleClientConnected(connectedClient) {
+  if (!isValidConnection(connectedClient)) {
+    connectedClient.close();
+  }
+
+  var resourceId = getResourceId(connectedClient);
+  observeResource(connectedClient, resourceId);
+
+  sendCurrentResourceDataToObserver(connectedClient, resourceId);
+}
+
+// Receive new resource data
+const resourceUpdatedSubscriber = zmq.socket('sub').connect('tcp://localhost:5433');
+
+function observeResource(connectedClient, resourceId) {
+  var redisClientSubscriber = resourceSubscribers[resourceId];
+
+  if (!redisClientSubscriber) {
+    log.silly('Creating a new Redis client for resource ' + resourceId);
+
+    redisClientSubscriber = redis.createClient();
+    resourceSubscribers[resourceId] = redisClientSubscriber;
+
+    resourceUpdatedSubscriber.subscribe(resourceId);
+  }
+
+  redisClientSubscriber.subscribe(resourceId, redis.print);
+
+  var sendDataToConnectedClient = function (channel, message) {
+    connectedClient.send(message);
+  };
+
+  redisClientSubscriber.addListener('message', sendDataToConnectedClient);
+
+  connectedClient.on('disconnect', function(){
+    redisClientSubscriber.removeListener('message', sendDataToConnectedClient);    
+  })
+
+  log.silly('Redis clients in memory: ' + Object.size(resourceSubscribers))
+  logNewObserver(resourceId);
+}
+
+function sendCurrentResourceDataToObserver(connectedClient, resourceId) {
+  // A promise here is not really needed but I like experimenting
+  Q.ninvoke(redisClient, 'get', resourceId)
+    .then(function(resourceData) {
+
+      if (resourceData) {
+        connectedClient.send(resourceData);  
+      } else {
+        requestResource(resourceId);
+      }
+    })
+    .catch(function (err) {
+      log.error('Cant send current resource data to observer ' +
+        'for resource ' + resourceId + ':' + err.stack);
+    })
+    .done();
+}
+
+// Publish a resource request for a resource that we don't have in Redis
+const resourceRequiredPusher = zmq.socket('push').bind('tcp://*:5432');
+
+resourceUpdatedSubscriber.on('message', function (data) {
+  handleResourceDataReceived(data);
+});
+
+function handleResourceDataReceived(data) {
+  var resource = JSON.parse(getJSONFromPublisherMessage(data)); 
+
+  log.verbose('Received resource data for resource ' + resource.id);
+
+  saveResourceData(resource);
+
+  notifyObservers(resource); 
+}
+
+/**
+ * Implementation of public endpoints
+ */
+
+function requestResource(resourceId) {
+  log.verbose('Requested resource (id: ' + resourceId + ') does not exist, sending a resource request');
+
+  resourceRequiredPusher.send(JSON.stringify({id: resourceId}));
+}
+
+function saveResourceData(resource) {
+  redisClient.set(resource.id, resource.data, redis.print);
+}
+
+function notifyObservers(resource) {
+  redisClient.publish(resource.id, resource.data);
+}
+
+function getResourceId(clientConnection) {
+  return clientConnection.request.query.resourceId;
+}
+
+function isValidConnection(clientConnection) {
+  var resourceId = getResourceId(clientConnection);
+
+  if (!resourceId) {
+    log.warn('Bad resource id (' + resourceId + ') is requested, closing the socket connection');
+    return false;
+  }
+
+  return true;
+}
+
+// Publisher messages are in format 'channnel message', in our case 'resourceId {resourceData}'
+function getJSONFromPublisherMessage(message) {
+  var messageAsString = String(message);
+
+  var indexOfJSON = messageAsString.indexOf('{');
+
+  return messageAsString.substring(indexOfJSON, message.length);
+}
+
+/**
+ * Logging
+ */
+
+function logNewObserver(resourceId) {
+  log.info('New connection for ' + resourceId + '. Total observers : ', io.clientsCount);
+}
+
+/**
+ * Graceful termination
+ */
+function closeAllConnections() {
+  resourceRequiredPusher.close();
+  resourceUpdatedSubscriber.close(); 
+  io.close();
+}
+
+process.on('uncaughtException', function (err) {
+  log.error('Caught exception: ' + err.stack);    
+  closeAllConnections();
+  process.exit(1);
+}); 
+
+process.on('SIGINT', function() {
+  log.warn('SIGINT detected, exiting gracefully.');
+  closeAllConnections();
+  process.exit();
+});
+
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
--- server.js
+++ server.js
@@ -1,202 +1,282 @@
 'use strict';
 
-const express = require('express'),
-  app = express(),
-  http = require('http'),
-  engine = require('engine.io'),
-  zmq = require('zmq'),
-  redis = require('redis'),
-  redisClient = redis.createClient(),
-  Q = require('q'),
-  log = require('npmlog');
-  // strongloop = require('strong-agent').profile();
+const 
+    cluster = require('cluster'),
+    zmq = require('zmq'),
+    redis = require('redis'),
+    log = require('npmlog');
 
 log.level = process.env.LOGGING_LEVEL || 'verbose';
 
-// Set how many concurrent sockets http agent can have open per host
-http.globalAgent.maxSockets = Infinity;
-process.setMaxListeners(0);
-
-// http server
-const server = http.createServer(app);
-
-// WebSocket server
-const io = engine.attach(server);
-
-app.use(express.static(__dirname + '/'));
-
-app.get('/', function(req, res, next){
-  res.sendfile('index.html');
-});
-
-const port = process.env.PORT || 5000;
-
-server.listen(port, function(){
-  log.info('Web socket server (Worker ' + process.pid + ') is listening on ', port);
-});
-
 /**
- * Data structures
- */
+ * Master process
+ */ 
+if (cluster.isMaster) {
+  log.info('Master (' + process.pid + ') is online.');
 
- // These are (currently) redis clients subscribed to different channels
- var resourceSubscribers = {};
+  const redisClient = redis.createClient(),
+    strongloop = require('strong-agent').profile();
+    
+  const totalWorkerCount = require('os').cpus().length;
 
-/**
- * Public Endpoints
- */
- 
-io.on('connection', function (socket) {
-  handleClientConnected(socket);
-});
-
-function handleClientConnected(connectedClient) {
-  if (!isValidConnection(connectedClient)) {
-    connectedClient.close();
+  for (let i = 1; i <= totalWorkerCount; i++) {
+    log.info('Forking worker ' + i + ' out of ' + totalWorkerCount);
+    cluster.fork();  
   }
 
-  var resourceId = getResourceId(connectedClient);
-  observeResource(connectedClient, resourceId);
+  // Listen for workers to come online
+  cluster.on('online', function(worker) {
+    log.info('Worker ' + worker.id + ' (pid: ' + worker.process.pid + ') is online.');
+  });
 
-  sendCurrentResourceDataToObserver(connectedClient, resourceId);
-}
+  // Handle dead workers
+  cluster.on('exit', function(worker, code, signal) {
+    log.warn('Worker ' + worker.id + ' (pid: ' + worker.process.pid + ') died with code ' + 
+      code + '. Forking a new one..');
+    this.fork();
+  });
 
-// Receive new resource data
-const resourceUpdatedSubscriber = zmq.socket('sub').connect('tcp://localhost:5433');
+  // Receive a resource request for a resource that workers don't have
+  const resourceRequiredPuller = zmq.socket('pull').bind('ipc://resource-required-puller.ipc', socketErrorHandler);
 
-function observeResource(connectedClient, resourceId) {
-  var redisClientSubscriber = resourceSubscribers[resourceId];
+   // Push a resource request for a resource that workers don't have
+  const resourceRequiredPusher = zmq.socket('push').bind('tcp://*:5432');
 
-  if (!redisClientSubscriber) {
-    log.silly('Creating a new Redis client for resource ' + resourceId);
+  // Receive new resource data
+  const resourceUpdatedSubscriber = zmq.socket('sub').connect('tcp://localhost:5433', socketErrorHandler);
 
-    redisClientSubscriber = redis.createClient();
-    resourceSubscribers[resourceId] = redisClientSubscriber;
+  resourceRequiredPuller.on('message', function (message) {
+    handleResourceRequested(message);    
+  });
+
+  function handleResourceRequested(message) {
+    var resourceId = JSON.parse(message).id;
+    
+    requestResource(resourceId)
+  }
+
+  function requestResource(resourceId) {
+    log.verbose('Master | Sending a resource request to Fetcher');
+
+    resourceRequiredPusher.send(JSON.stringify({id: resourceId}));
 
     resourceUpdatedSubscriber.subscribe(resourceId);
   }
 
-  redisClientSubscriber.subscribe(resourceId, redis.print);
+  resourceUpdatedSubscriber.on('message', function (data) {
+    handleResourceDataReceived(data);
+  });
 
-  var sendDataToConnectedClient = function (channel, message) {
-    connectedClient.send(message);
-  };
+  function handleResourceDataReceived(data) {
+    var resource = JSON.parse(getJSONFromPublisherMessage(data)); 
 
-  redisClientSubscriber.addListener('message', sendDataToConnectedClient);
+    log.verbose('Master | Received resource data for resource ' + resource.id);
 
-  connectedClient.on('disconnect', function(){
-    redisClientSubscriber.removeListener('message', sendDataToConnectedClient);    
-  })
+    saveResourceData(resource);
 
-  log.silly('Redis clients in memory: ' + Object.size(resourceSubscribers))
-  logNewObserver(resourceId);
-}
-
-function sendCurrentResourceDataToObserver(connectedClient, resourceId) {
-  // A promise here is not really needed but I like experimenting
-  Q.ninvoke(redisClient, 'get', resourceId)
-    .then(function(resourceData) {
-
-      if (resourceData) {
-        connectedClient.send(resourceData);  
-      } else {
-        requestResource(resourceId);
-      }
-    })
-    .catch(function (err) {
-      log.error('Cant send current resource data to observer ' +
-        'for resource ' + resourceId + ':' + err.stack);
-    })
-    .done();
-}
-
-// Publish a resource request for a resource that we don't have in Redis
-const resourceRequiredPusher = zmq.socket('push').bind('tcp://*:5432');
-
-resourceUpdatedSubscriber.on('message', function (data) {
-  handleResourceDataReceived(data);
-});
-
-function handleResourceDataReceived(data) {
-  var resource = JSON.parse(getJSONFromPublisherMessage(data)); 
-
-  log.verbose('Received resource data for resource ' + resource.id);
-
-  saveResourceData(resource);
-
-  notifyObservers(resource); 
-}
-
-/**
- * Implementation of public endpoints
- */
-
-function requestResource(resourceId) {
-  log.verbose('Requested resource (id: ' + resourceId + ') does not exist, sending a resource request');
-
-  resourceRequiredPusher.send(JSON.stringify({id: resourceId}));
-}
-
-function saveResourceData(resource) {
-  redisClient.set(resource.id, resource.data, redis.print);
-}
-
-function notifyObservers(resource) {
-  redisClient.publish(resource.id, resource.data);
-}
-
-function getResourceId(clientConnection) {
-  return clientConnection.request.query.resourceId;
-}
-
-function isValidConnection(clientConnection) {
-  var resourceId = getResourceId(clientConnection);
-
-  if (!resourceId) {
-    log.warn('Bad resource id (' + resourceId + ') is requested, closing the socket connection');
-    return false;
+    notifyObservers(resource); 
   }
 
-  return true;
+  function saveResourceData(resource) {
+    redisClient.set(resource.id, resource.data, redis.print);
+  }
+
+  function notifyObservers(resource) {
+    redisClient.publish(resource.id, resource.data);
+  }
+
+  // Publisher messages are in format 'channnel message', in our case 'resourceId {resourceData}'
+  function getJSONFromPublisherMessage(message) {
+    var messageAsString = String(message);
+
+    var indexOfJSON = messageAsString.indexOf('{');
+
+    return messageAsString.substring(indexOfJSON, message.length);
+  }
+
+  /**
+   * Graceful termination
+   */
+  function closeAllConnections() {
+    resourceRequiredPuller.close();
+    resourceRequiredPusher.close(); 
+    resourceUpdatedSubscriber.close(); 
+  }
+
+  process.on('uncaughtException', function (err) {
+    log.error('Master | Caught exception: ' + err.stack);    
+    closeAllConnections();
+    process.exit(1);
+  }); 
+
+  process.on('SIGINT', function() {
+    log.warn('Master | SIGINT detected, exiting gracefully.');
+    closeAllConnections();
+    process.exit();
+  });
+
+} else {
+  /**
+   * Worker process
+   */
+
+  const express = require('express'),
+    app = express(),
+    http = require('http'),
+    engine = require('engine.io'),
+    redisClient = redis.createClient(),
+    Q = require('q');
+
+  // Set how many concurrent sockets http agent can have open per host
+  http.globalAgent.maxSockets = Infinity;
+  process.setMaxListeners(0);
+
+  // http server
+  const server = http.createServer(app);
+
+  // WebSocket server
+  const io = engine.attach(server);
+
+  app.use(express.static(__dirname + '/'));
+
+  app.get('/', function(req, res, next){
+    res.sendfile('index.html');
+  });
+
+  const port = process.env.PORT || 5000;
+
+  server.listen(port, function(){
+    log.info('Web socket server (Worker ' + cluster.worker.id + ' - pid: ' + process.pid + ') is listening on ', port);
+  });
+
+  /**
+   * Data structures
+   */
+
+   // These are (currently) redis clients subscribed to different channels
+   var resourceSubscribers = {};
+
+  /**
+   * Public Endpoints
+   */
+   
+  io.on('connection', function (socket) {
+    handleClientConnected(socket);
+  });
+
+  function handleClientConnected(connectedClient) {
+    if (!isValidConnection(connectedClient)) {
+      connectedClient.close();
+    }
+
+    var resourceId = getResourceId(connectedClient);
+    observeResource(connectedClient, resourceId);
+
+    sendCurrentResourceDataToObserver(connectedClient, resourceId);
+  }
+
+  function observeResource(connectedClient, resourceId) {
+    var redisClientSubscriber = resourceSubscribers[resourceId];
+
+    if (!redisClientSubscriber) {
+      log.silly('W' + cluster.worker.id + ' | Creating a new Redis client for resource ' + resourceId);
+
+      redisClientSubscriber = redis.createClient();
+      resourceSubscribers[resourceId] = redisClientSubscriber;
+    }
+
+    redisClientSubscriber.subscribe(resourceId, redis.print);
+
+    redisClientSubscriber.on('message', function(channel, message) {
+        connectedClient.send(message);
+    });
+
+    log.silly('W' + cluster.worker.id + ' | Redis clients in memory: ' + Object.size(resourceSubscribers))
+    logNewObserver(resourceId);
+  }
+
+  function sendCurrentResourceDataToObserver(connectedClient, resourceId) {
+    // A promise here is not really needed but I like experimenting
+    Q.ninvoke(redisClient, 'get', resourceId)
+      .then(function(resourceData) {
+
+        if (resourceData) {
+          connectedClient.send(resourceData);  
+        } else {
+          requestResource(resourceId);
+        }
+      })
+      .catch(function (err) {
+        log.error('W' + cluster.worker.id+ ' | Cant send current resource data to observer ' +
+          'for resource ' + resourceId + ':' + err.stack);
+      })
+      .done();
+  }
+
+  // Publish a resource request for a resource that we don't have in Redis
+  const resourceRequiredPusher = zmq.socket('push').connect('ipc://resource-required-puller.ipc', socketErrorHandler);
+
+  /**
+   * Implementation of public endpoints
+   */
+
+  function requestResource(resourceId) {
+    log.verbose('W' + cluster.worker.id + ' | Requested resource (id: ' + resourceId + ') does not exist, sending a resource request to Master');
+
+    resourceRequiredPusher.send(JSON.stringify({id: resourceId}));
+  }
+
+  function getResourceId(clientConnection) {
+    return clientConnection.request.query.resourceId;
+  }
+
+  function isValidConnection(clientConnection) {
+    var resourceId = getResourceId(clientConnection);
+
+    if (!resourceId) {
+      log.warn('W' + cluster.worker.id + ' | Bad resource id (' + resourceId + ') is requested, closing the socket connection');
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Logging
+   */
+
+  function logNewObserver(resourceId) {
+    log.info('W' + cluster.worker.id + ' | New connection for ' + resourceId + '. Total observers : ', io.clientsCount);
+  }
+
+  /**
+   * Graceful termination
+   */
+  function closeAllConnections() {
+    resourceRequiredPusher.close();
+    io.close();
+  }
+
+  process.on('uncaughtException', function (err) {
+    log.error('W' + cluster.worker.id + ' (pid: ' + worker.process.pid + ') | Caught exception: ' + err.stack);    
+    closeAllConnections();
+    process.exit(1);
+  }); 
+
+  process.on('SIGINT', function() {
+    closeAllConnections();
+    process.exit();
+  });
+
 }
 
-// Publisher messages are in format 'channnel message', in our case 'resourceId {resourceData}'
-function getJSONFromPublisherMessage(message) {
-  var messageAsString = String(message);
-
-  var indexOfJSON = messageAsString.indexOf('{');
-
-  return messageAsString.substring(indexOfJSON, message.length);
-}
-
-/**
- * Logging
- */
-
-function logNewObserver(resourceId) {
-  log.info('New connection for ' + resourceId + '. Total observers : ', io.clientsCount);
-}
-
-/**
- * Graceful termination
- */
-function closeAllConnections() {
-  resourceRequiredPusher.close();
-  resourceUpdatedSubscriber.close(); 
-  io.close();
-}
-
-process.on('uncaughtException', function (err) {
-  log.error('Caught exception: ' + err.stack);    
-  closeAllConnections();
-  process.exit(1);
-}); 
-
-process.on('SIGINT', function() {
-  log.warn('SIGINT detected, exiting gracefully.');
-  closeAllConnections();
-  process.exit();
-});
+var socketErrorHandler = function (err) {
+    if (err) {
+      log.error('Socket connection error: ' + err.stack);
+      throw new Error(err);
+    }
+    log.info('Socket open.');
+};
 
 Object.size = function(obj) {
     var size = 0, key;
